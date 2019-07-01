1eac1c7ad9c9d313b3c1a9cc5426722ca98daaa3
codfish <392283100@qq.com>
2019-07-01 17:03:25 +0800

    1

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public void StartBtn_LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LevelBtn_LoadScene(int id)
+    {
+        SceneManager.LoadScene(id);
+    }
+}
