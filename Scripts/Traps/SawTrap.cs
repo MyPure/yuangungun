00fd35e445a6eb2a43830b9b5ccc2e3ada73fb05
codfish <392283100@qq.com>
2019-07-02 15:29:52 +0800

    do nothing

--- Scripts/Traps/SawTrap.cs
+++ Scripts/Traps/SawTrap.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class SawTrap : MonoBehaviour
 {
-    public Collider trapCollider;
-
-    private void OnTriggerEnter(Collider other)
+    public GameObject saw;
+    public float sawSpeed;
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        Debug.Log(1);
+        saw.SetActive(true);
+        saw.GetComponent<Rigidbody2D>()
+            .AddForce(Vector2.left* Time.deltaTime * sawSpeed, ForceMode2D.Impulse);
+    }
+
+    private void Update()
+    {
+        if (saw.activeSelf == true)
+        {
+            Vector3 vector3 = transform.position + Vector3.forward;
+            saw.transform.Rotate(vector3, 5);            
+        }
     }
 }
