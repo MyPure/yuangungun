08a54c7ef1a4d107b7368495029d0a9e0872473a
My_Pure <cyn761480091@qq.com>
2019-07-04 10:31:11 +0800

    优化教程关

---
+++ Scripts/BubbleTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleTrigger : MonoBehaviour
+{
+    public GameObject bubble;
+    public TextMesh bubbletext;
+    public string text;
+    private void Start()
+    {
+        bubble.SetActive(false);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            bubble.SetActive(true);
+            bubbletext.text = text;
+            bubble.GetComponent<Animator>().Play("气泡_1");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            bubble.GetComponent<Animator>().Play("气泡_3");
+        }
+    }
+}
