055b4ac6a60ae592465804d8deab55f94b2e1bc7
codfish <392283100@qq.com>
2019-07-05 16:54:15 +0800

    继续做第二关

--- Scripts/Traps/MovingCube.cs
+++ Scripts/Traps/MovingCube.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MovingCube : MonoBehaviour
 {
+    private bool stop = true;
+    public bool trigger=false;
+    public bool isElevator;
     public Transform[] wayPoints;
     public float speed;
     private int index = 0;
@@ -17,10 +20,28 @@ public class MovingCube : MonoBehaviour
             if (index == 0) change = 1;
             else if (index == wayPoints.Length-1) change = -1;
             index+=change;
+            stop = !stop;
         }
     }
+
+    private void ElevatorMove()
+    {
+        if (!stop && trigger)
+        {
+            Move();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (trigger)
+        {
+            stop = false;
+        }
+    }
+
     private void Update()
     {
-        Move();
+        if (isElevator) ElevatorMove();
+        else Move();
     }
 }
